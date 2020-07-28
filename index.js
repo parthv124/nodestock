0a995a419dd232df77bb429e6699b8392756476f
parth vashishtha <parthv8291@gmail.com>
2020-07-28 07:58:38 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home',{
+    	stuff:"this is stuff..."
+    });
+});
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(PORT, () => console.log('Server listening on port '+ PORT));
